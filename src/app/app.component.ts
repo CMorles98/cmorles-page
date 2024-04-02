8ac40d60a147441512a33b6c6832c29019948a52
CMorles98 <christopher.mh98@gmail.com>
2024-04-02 01:43:59 -0400

    add spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import AOS from 'aos';
+import { NgxSpinner, NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    NavBarComponent, 
+    FooterComponent,
+    NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
@@ -16,8 +22,8 @@ import AOS from 'aos';
 })
 export class AppComponent {
   title = 'cmorles-app';
-
-  constructor(){
+  constructor(spinner: NgxSpinnerService){
+    spinner.show()
     AOS.init()
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PdfViewerModule],
+  imports: [
+    PdfViewerModule,
+    NgxSpinnerModule  
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+  ngAfterViewInit(): void {
+    this.spinner.hide()
+  }
   
 }
